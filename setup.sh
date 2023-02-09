555abfb9fdbd1ecddaf6d911dc111a90e0300335
Letder40 <adrian04rv@gmail.com>
2023-02-10 00:51:48 +0100

    bugs fixed

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ apt update
 apt install docker
 apt install docker-compose
 
-ip=$(ip a | grep "ens33" | grep -oP "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | grep -v ".255")
-cat ./login-docker/db.php | sed "s/ip.addr/$ip/g"
-cat ./registro-docker/db.php | sed "s/ip.addr/$ip/g"
+ip=$(ip a | grep "eth0" | grep -oP "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | grep -v ".255")
+cat ./login-docker/public_html/db.php | sed "s/ip.addr/$ip/g"
+cat ./registro-docker/public_html/db.php | sed "s/ip.addr/$ip/g"
 cat ./wordpress-docker/public_html/wp-config.php | sed "s/ip.addr/$ip/g"
