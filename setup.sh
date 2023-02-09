76685b3f1bd0211220233ba9a5a976a21f03c770
Letder40 <adrian04rv@gmail.com>
2023-02-10 00:57:24 +0100

    setup.sh finalized 2

--- setup.sh
+++ setup.sh
@@ -7,4 +7,6 @@ apt install moreutils
 ip=$(ip a | grep "eth0" | grep -oP "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | grep -v ".255")
 cat ./login-docker/public_html/db.php | sed "s/ip.addr/$ip/g" | sponge ./login-docker/public_html/db.php 
 cat ./registro-docker/public_html/db.php | sed "s/ip.addr/$ip/g" | sponge ./registro-docker/public_html/db.php 
-cat ./wordpress-docker/public_html/wp-config.php | sed "s/ip.addr/$ip/g" | sponge ./wordpress-docker/public_html/wp-config.php 
+cat ./wordpress-docker/public_html/wp-config.php | sed "s/ip.addr/$ip/g" | sponge ./wordpress-docker/public_html/wp-config.php
+
+bash autodocker.sh
